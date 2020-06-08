d77dd610ed9196fd06c08d9a649256848ceee872
PadmaGnanapriya <padmaisuru@gmail.com>
2020-06-08 09:37:53 +0530

    Create welcome screen

--- kernel.c
+++ kernel.c
@@ -27,5 +27,5 @@ kmain()
 kmain()
 {       
         clearScreen();
-        print("Hi, And welcome to the IknowOS\nPlease don't forget to subscribe to https://www.youtube.com/user/iknowbrain\nThank you");
+        print("\n\tWelcome to Padma-OS");
 }
