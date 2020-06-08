0146f3b2a1fe73de598c4d6d8566d69c5b0d90d5
PadmaGnanapriya <padmaisuru@gmail.com>
2020-06-08 08:51:08 +0530

    modify code

--- kernel.c
+++ kernel.c
@@ -1,6 +1,22 @@
 kmain()
 {
+    
     char* vidmen =(char*)0xb8000;
-    vidmen[0]='P';
-    vidmen[1]=0x02;
+    vidmen[0]='*';
+    vidmen[1]=0x01;
+    vidmen[2]='P';
+    vidmen[3]=0x02;
+    vidmen[4]='A';
+    vidmen[5]=0x03;
+    vidmen[6]='D';
+    vidmen[7]=0x04;
+    vidmen[8]='M';
+    vidmen[9]=0x05;
+    vidmen[10]='A';
+    vidmen[11]=0x06;
+    vidmen[12]='O';
+    vidmen[13]=0x07;
+    vidmen[14]='S';
+    vidmen[15]=0x08;
+    
 }
